94433476ceb2e31cab1e23ad8fc88ae0b71f6bf5
yaserwang <wangchao.2007@sis.smu.edu.sg>
2018-05-29 10:49:23 +0800

    only expose the specified package

--- src/main/java/com/sp/SwaggerConfig.java
+++ src/main/java/com/sp/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.sp.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
